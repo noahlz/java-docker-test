8c005515f2add79dc4e68f9db65ddc608ce6b05d
Noah Zucker <nzucker@novus.com>
2020-04-09 00:06:12 -0400

    Additionally, test tha JVM actually tries to use the cores it thinks it has

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Runtime.getRuntime.availableProcessors() = " + Runtime.getRuntime().availableProcessors());
-    System.out.println("java.util.concurrent.ForkJoinPool.getCommonPoolParallelism() = " + java.util.concurrent.ForkJoinPool.getCommonPoolParallelism());
+  public static void main(String[] args) throws InterruptedException {
+    int cores = Runtime.getRuntime().availableProcessors();
+    int parallelism = java.util.concurrent.ForkJoinPool.getCommonPoolParallelism();
+
+    System.out.println("Runtime.getRuntime.availableProcessors() = " + cores);
+    System.out.println("java.util.concurrent.ForkJoinPool.getCommonPoolParallelism() = " + parallelism);
+
+    List<Integer> nums = new ArrayList<>();
+    for (Integer i = 1; i < 1000; i++) {
+      nums.add(i);
+    }
+
+    System.out.println("Now checking if we really have " + cores + " core(s)...");
+    Thread.sleep(5000L);
+
+    nums.parallelStream().forEach(n -> System.out.println(Thread.currentThread().getName() + ": " + n));
   }
 }
